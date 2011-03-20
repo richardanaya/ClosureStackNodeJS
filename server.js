c605c8fac67acb64d31cf720249098dc6c5c528d
Richard Anaya <richard.anaya@gmail.com>
2011-03-19 22:47:23 -0500

    removing DS_Store

--- server.js
+++ server.js
@@ -24,28 +24,3 @@ app.get(/^.*$/, function(req, res){
 });
 
 app.listen(9999);
-
-//WebSocket Setup, delete below this line if you don't use
-var io = require('socket.io');
-var clients = [];
-
-var socket = io.listen(app); 
-socket.on('connection', function(client){ 
-  clients.push(client);
-  client.on('message', function(data){ 
-    console.log('sending '+data+' to all clients'); 
-    for(c in clients) {
-      client.send(data);
-    }
-  });
-  client.on('disconnect', function(){
-    console.log('client disconnected');
-    for(var i = 0,len=clients.length;i<len; i++){
-      if(clients[i] == client) {
-        clients.splice(i,1);
-        break;
-      }
-    } 
-  });
-}); 
-
