a6233dec31a354f8acc12884cad76d63a8915fed
Richard Anaya <richard.anaya@gmail.com>
2011-03-19 22:58:15 -0500

    can now convert any file named soy

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-java -jar closure-tools/SoyToJsSrcCompiler.jar --outputPathFormat static/js/hello.soy.js --shouldGenerateJsdoc --shouldProvideRequireSoyNamespaces static/js/hello.soy 
+find static/js -name *.soy -exec java -jar closure-tools/SoyToJsSrcCompiler.jar --outputPathFormat {}.js --shouldGenerateJsdoc --shouldProvideRequireSoyNamespaces {} \;
 find static/js -name *.coffee -exec coffee -c {} \;
 python closure-tools/closure/bin/build/closurebuilder.py --root closure-tools --root static --namespace "website" --output_mode=script --compiler_jar=closure-tools/compiler.jar > static/js/website-compiled.js
