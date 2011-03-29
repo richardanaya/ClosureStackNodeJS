cd6fc697ab4ed237429d1091a6eaf507f4d7a6a5
Richard Anaya <richard.anaya@gmail.com>
2011-03-29 14:42:29 -0500

    fixing jsdoc generation for new client server moel

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+./compile.sh
 cd jsdoc
-java -jar jsrun.jar app/run.js --private -t=templates/jsdoc -d=../docs ../website/src/*.js
+java -jar jsrun.jar app/run.js --private -t=templates/jsdoc -d=../docs/client ../website/client/*.js
+java -jar jsrun.jar app/run.js --private -t=templates/jsdoc -d=../docs/server ../website/server/*.js
 cd ..
